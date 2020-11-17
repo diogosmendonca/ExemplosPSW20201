636c1d04375e4503a2e54828da2957c187e3c966
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-11-17 09:30:24 -0300

    Alguns casos de teste de defeito sem implementação

---
+++ src/projetos/FormProjeto.test.js
@@ -0,0 +1,17 @@
+
+//####### CAMPO NOME ################################
+
+test('Nome vazio', () => {throw 'Not implemented yet'});
+
+test('Nome limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Nome válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO SIGLA ################################
+
